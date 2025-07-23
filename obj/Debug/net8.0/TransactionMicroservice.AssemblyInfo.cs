f3b4960eb55d4731e190f400c60728555d6b2362
Lucas Matos <abreulucas.dias@gmail.com>
2025-07-22 23:31:14 -0400

    feat(api): Adiciona endpoint para listar transações
    Implementa o endpoint GET /transacoes para retornar uma lista mockada de transações.

--- obj/Debug/net8.0/TransactionMicroservice.AssemblyInfo.cs
+++ obj/Debug/net8.0/TransactionMicroservice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransactionMicroservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f37b31354e9eadfdce222d3a6a04667d271282d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransactionMicroservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransactionMicroservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
